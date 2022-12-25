c65e3e166a1c127824028a66329ab6a9ba6d4835
SuitedIceZ <okshopm575@gmail.com>
2022-12-25 23:26:31 +0700

    Learning controller, service amd module

--- first-project/src/cats/cats.controller.ts
+++ first-project/src/cats/cats.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get , Post , HttpCode , Req , Body } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+
+  @Post()
+  @HttpCode(201)
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 }
